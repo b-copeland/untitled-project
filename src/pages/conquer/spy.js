f1109f336801b006a2a387ea41d465323953d43f
Ben Copeland <b.copeland92@gmail.com>
2023-03-19 11:48:20 -0400

    Add revealed stats on spy page
    To give more context before trying to reveal more

--- src/pages/conquer/spy.js
+++ src/pages/conquer/spy.js
@@ -129,6 +129,29 @@ function Spy(props) {
             <Toast.Body>{results.message}</Toast.Body>
         </Toast>
     )
+    function getTimeString(date) {
+        if (date === undefined) {
+            return "--"
+        }
+        const hours = Math.abs(Date.parse(date) - Date.now()) / 3.6e6;
+        var n = new Date(0, 0);
+        n.setSeconds(+hours * 60 * 60);
+        return n.toTimeString().slice(0, 8);
+    }
+    const getRemainingSpans = (kdId, revealed) => {
+        if (revealed === undefined) {
+            return []
+        }
+        const remainingSpans = Object.keys(revealed[kdId] || {}).map((category) =>
+            <div key={kdId.toString() + '_' + category} className="remaining-timer">
+                <span className="remaining-time-title">{category}</span>
+                <span className="remaining-time-value">{getTimeString(revealed[kdId][category])}</span>
+                <br />
+            </div>
+        )
+        return remainingSpans;
+    }
+    const revealedStats = getRemainingSpans(selected, props.data.revealed?.revealed);
     if (drones > props.data.kingdom["drones"]) {
         setDrones(Math.floor(props.data.kingdom["drones"]))
     }
@@ -309,6 +332,14 @@ function Spy(props) {
                             </Button>
                         }
                     </div>
+                    {
+                        revealedStats.length > 0
+                        ? <div className="text-box revealed-stats-box">
+                            <h3>Target's Revealed Stats</h3>
+                            {revealedStats}
+                        </div>
+                        : null
+                    }
                 </div>
             </div>
         </div>
