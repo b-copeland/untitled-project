dbc64f7c5f3c1cb0cacfd01a8a756b8af0585069
Ben Copeland <b.copeland92@gmail.com>
2023-04-28 22:10:35 -0400

    Fix existing percent settings being divided by 100

--- api/api.py
+++ api/api.py
@@ -4562,10 +4562,10 @@ def auto_attack_primitives():
     
     req_settings = {
         "pure": float(
-            req.get("pure", kd_info_parse["auto_attack_settings"].get("pure", 0)) or 0
+            req.get("pure", (kd_info_parse["auto_attack_settings"].get("pure", 0) * 100) or 0)
         ) / 100,
         "flex": float(
-            req.get("flex", kd_info_parse["auto_attack_settings"].get("flex", 0)) or 0
+            req.get("flex", (kd_info_parse["auto_attack_settings"].get("flex", 0)) * 100) or 0
         ) / 100,
     }
 
@@ -5132,7 +5132,7 @@ def auto_rob_primitives():
     
     req_settings = {
         "drones": float(
-            req.get("drones", kd_info_parse["auto_rob_settings"].get("drones", 0)) or 0
+            req.get("drones", (kd_info_parse["auto_rob_settings"].get("drones", 0) * 100)) or 0
         ) / 100,
         "keep": int(
             req.get("keep", kd_info_parse["auto_rob_settings"].get("keep", 0)) or 0
